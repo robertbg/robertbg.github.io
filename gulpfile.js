7c417b1be5ef4ddb44c058481e7e8a16eb8efc3a
Rob BG <rob.bg@me.com>
2016-11-28 13:51:30 +0000

    Gulp svg process and added logos

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
+const path = require('path');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const inlineSource = require('gulp-inline-source');
+const svgstore = require('gulp-svgstore');
+const svgmin = require('gulp-svgmin');
+const inject = require('gulp-inject');
 
 gulp.task('sass', () => gulp.src('./src/scss/**/*.scss')
   .pipe(sass().on('error', sass.logError))
@@ -12,6 +16,30 @@ gulp.task('html', ['sass'], () => gulp.src('./src/*.html')
   }))
   .pipe(gulp.dest('')));
 
-gulp.task('watch', () => gulp.watch('./src/**/*.*', ['html']));
+gulp.task('svg', ['html'], () => {
+  const svgs = gulp.src('svg/*.svg')
+  .pipe(svgmin((file) => {
+    const prefix = path.basename(file.relative, path.extname(file.relative));
+    return {
+      plugins: [{
+        cleanupIDs: {
+          prefix: prefix + '-',
+          minify: true
+        }
+      }]
+    }
+  }))
+  .pipe(svgstore({ inlineSvg: true }));
+  
+  function fileContents (filePath, file) {
+    return file.contents.toString();
+  }
+  
+  return gulp.src('index.html')
+  .pipe(inject(svgs, { transform: fileContents }))
+  .pipe(gulp.dest(''));
+});
 
-gulp.task('default', ['html']);
+gulp.task('watch', () => gulp.watch('./src/**/*.*', ['svg']));
+
+gulp.task('default', ['svg']);
