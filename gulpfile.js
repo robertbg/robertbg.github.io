fc7d95b5cd29ea2e4e9196a19331359f0d7c48e2
Rob BG <rob.bg@me.com>
2016-11-28 15:20:13 +0000

    Added svg logos for brands I’ve worked with

--- gulpfile.js
+++ gulpfile.js
@@ -2,44 +2,31 @@ const path = require('path');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const inlineSource = require('gulp-inline-source');
-const svgstore = require('gulp-svgstore');
 const svgmin = require('gulp-svgmin');
-const inject = require('gulp-inject');
+const injectSvg = require('gulp-inject-svg');
 
 gulp.task('sass', () => gulp.src('./src/scss/**/*.scss')
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./dist')));
+  .pipe(gulp.dest('./dist'))
+);
 
-gulp.task('html', ['sass'], () => gulp.src('./src/*.html')
+gulp.task('inject-html', ['sass'], () => gulp.src('./src/*.html')
   .pipe(inlineSource({
       compress: true
   }))
-  .pipe(gulp.dest('')));
+  .pipe(gulp.dest(''))
+);
 
-gulp.task('svg', ['html'], () => {
-  const svgs = gulp.src('svg/*.svg')
-  .pipe(svgmin((file) => {
-    const prefix = path.basename(file.relative, path.extname(file.relative));
-    return {
-      plugins: [{
-        cleanupIDs: {
-          prefix: prefix + '-',
-          minify: true
-        }
-      }]
-    }
-  }))
-  .pipe(svgstore({ inlineSvg: true }));
-  
-  function fileContents (filePath, file) {
-    return file.contents.toString();
-  }
-  
-  return gulp.src('index.html')
-  .pipe(inject(svgs, { transform: fileContents }))
-  .pipe(gulp.dest(''));
-});
+gulp.task('min-svg', () =>  gulp.src('./svg/*.svg')
+  .pipe(svgmin())
+  .pipe(gulp.dest('./svg'))
+);
 
-gulp.task('watch', () => gulp.watch('./src/**/*.*', ['svg']));
+gulp.task('inject-svg', ['inject-html'], () => gulp.src('index.html')
+  .pipe(injectSvg())
+  .pipe(gulp.dest(''))
+);
 
-gulp.task('default', ['svg']);
+gulp.task('watch', () => gulp.watch('./src/**/*.*', ['inject-svg']));
+
+gulp.task('default', ['inject-svg']);
