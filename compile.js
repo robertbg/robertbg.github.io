06311b11073112296dca0426e5f73079f7cbc013
Rob BG <rob.bg@me.com>
2017-03-19 15:27:10 +0000

    Added comments

--- compile.js
+++ compile.js
@@ -20,105 +20,84 @@ const paths = {
 // Array to store blog posts
 const globalItems = [];
 
-// Markdown setup
-const markedOpts = {
-  gfm: true
-}
+// Markdown options
+const markedOpts = { gfm: true };
 
 // Register partials
 const registerPartials = () => {
-  const files = fs.readdirSync(paths.partialPath);
-  let content;
-  let filename;
+  const files = fs.readdirSync(paths.partialPath); // Read partials
 
   files.forEach((file) => {
-    content = fs.readFileSync(`${paths.partialPath}/${file}`, 'utf8');
-    filename = file.substring(0, file.lastIndexOf('.'));
+    let content = fs.readFileSync(`${paths.partialPath}/${file}`, 'utf8');
+    let filename = file.substring(0, file.lastIndexOf('.'));
     handlebars.registerPartial(filename, content);
   });
 };
 
-// Register helpers
-const registerHelpers = () => {
-  handlebars.registerHelper('dateFormat', dateformat);
-};
+// Register date format helper
+const registerHelpers = () => handlebars.registerHelper('dateFormat', dateformat);
 
 // Inline css and js
 const inlineSource = (html) => inline.sync(html, { compress: true });
 
 // Renders the template using Handlebars
 const renderTemplate = (templatePath, viewData) => {
-  const content = fs.readFileSync(templatePath, { encoding: 'utf-8' });
-  const template = handlebars.compile(content);
-  return inlineSource(template(viewData));
+  const content = fs.readFileSync(templatePath, { encoding: 'utf-8' }); // Read path
+  const template = handlebars.compile(content); // Compile template
+  return inlineSource(template(viewData)); // Return inline templated data
 };
 
 // Render and write
 const writeToFile = (template, data, path) => {
-  let HTML = renderTemplate(template, data);
-  mkpath.sync(path);
-  fs.writeFileSync(`${path}/index.html`, HTML, { encoding: 'utf8' });
+  let HTML = renderTemplate(template, data); // Render template
+  mkpath.sync(path); // Make sure path exists
+  fs.writeFileSync(`${path}/index.html`, HTML, { encoding: 'utf8' }); // Write to file
 };
 
 // Builds email content and writes to file.
 const buildSingleHTML = (content) => {
-  // Converts markdown file into a javascript object// Converts markdown body into html
-  const contentData = fm(content);
-  const formattedDate = moment(contentData.attributes.date).format('YYYY-MM-DD');
-
-  // HTML Version Paths
-  const htmlPath = `${paths.buildPath}blog/${formattedDate}-${contentData.attributes.fileName}`;
-  const htmlTemplate = `${paths.templatePath}/${contentData.attributes.template}`;
-
- // Converts markdown body into html
-  contentData.bodyFormatted = marked(contentData.body, markedOpts);
-
-  // Push into global array for rendering
-  globalItems.push(contentData);
-
-  // Render the HTML content and write to file
-  writeToFile(htmlTemplate, contentData, htmlPath);
+  const data = fm(content); // Front matter the content
+  const formattedDate = moment(data.attributes.date).format('YYYY-MM-DD'); // Format date
+  const htmlPath = `${paths.buildPath}blog/${formattedDate}-${data.attributes.fileName}`; // Build path
+  const htmlTemplate = `${paths.templatePath}/${data.attributes.template}`; // Template path
+  data.bodyFormatted = marked(data.body, markedOpts); // Converts markdown body into html 
+  globalItems.push(data); // Push into global array for blog index
+  writeToFile(htmlTemplate, data, htmlPath); // Render the HTML content and write to file
 };
 
 // Build HTML for homepage
 const buildHomeHTML = () => {
-  const content = fs.readFileSync(`${paths.globalPath}/homepage.md`, { encoding: 'utf8' });
-  const globalData = fm(content);
-  const htmlTemplate = `${paths.templatePath}/${globalData.attributes.template}`;
-
-  globalData.bodyFormatted = marked(globalData.body, markedOpts);
-
-  // Render the HTML content and write to file
-  writeToFile(htmlTemplate, globalData, paths.buildPath);
+  const content = fs.readFileSync(`${paths.globalPath}/homepage.md`, { encoding: 'utf8' }); // Read content path
+  const data = fm(content); // Front matter the content
+  const htmlTemplate = `${paths.templatePath}/${data.attributes.template}`; // Template path
+  data.bodyFormatted = marked(data.body, markedOpts); // Converts markdown body into html
+  writeToFile(htmlTemplate, data, paths.buildPath); // Render the HTML content and write to file
 };
 
 // Build HTML for blog listing page
 const buildBlogHTML = () => {
-  const content = fs.readFileSync(`${paths.globalPath}/blog.md`, { encoding: 'utf8' });
-  const globalData = fm(content);
-  const htmlTemplate = `${paths.templatePath}/${globalData.attributes.template}`;
-
-  globalData.bodyFormatted = marked(globalData.body, markedOpts);
-  globalData.items = globalItems.sort((a, b) => {
+  const content = fs.readFileSync(`${paths.globalPath}/blog.md`, { encoding: 'utf8' }); // Read content path
+  const data = fm(content); // Front matter the content
+  const htmlTemplate = `${paths.templatePath}/${data.attributes.template}`; // TEmpalte path
+  data.bodyFormatted = marked(data.body, markedOpts); // Converts markdown body into html
+  data.items = globalItems.sort((a, b) => { // Sort posts in alphabetical order
     a = new Date(a.attributes.date);
     b = new Date(b.attributes.date);
     return a > b ? -1 : a < b ? 1 : 0;
   });
-  
-  // Render the HTML content and write to file
-  writeToFile(htmlTemplate, globalData, `${paths.buildPath}/blog`);
+  writeToFile(htmlTemplate, data, `${paths.buildPath}/blog`); // Render the HTML content and write to file
 };
 
 // Build index pages
 const buildMainPages = () => {
-  buildBlogHTML();
-  buildHomeHTML();
+  buildBlogHTML(); // Blog index page
+  buildHomeHTML(); // Home page
 };
 
 // Run app
 (() => {
-  registerPartials(); // Register Partials
-  registerHelpers(); // Register Helpers
+  registerPartials(); // Register partials
+  registerHelpers(); // Register helpers
 
   // Uses 'node-dir' to recursively search through the contentPath directory.
   dir.readFiles(
@@ -129,10 +108,8 @@ const buildMainPages = () => {
     },
     (err, content, next) => {
       if (err) throw err;
-      buildSingleHTML(content);
-
-      // Moves on to the next content file
-      next();
+      buildSingleHTML(content); // Build html for each blog post
+      next(); // Moves on to the next content file
     },
     buildMainPages
   );
