d49a45ce3d585738f1b39ce18ef6d446be8f5b05
Rob Barns-Graham <r.barnsgraham@reply.com>
2017-03-30 17:18:44 +0100

    Squashed commit of the following:
    commit 101b6c04eed9982882fa9305dc33a6fd872363fd
    Author: Rob Barns-Graham <r.barnsgraham@reply.com>
    Date:   Thu Mar 30 17:17:58 2017 +0100
    
        Made compilation async
    
    commit 0c07cc488d74e4783fe51f73bb3fc5b6d8c7dba4
    Author: Rob Barns-Graham <r.barnsgraham@reply.com>
    Date:   Thu Mar 30 10:13:06 2017 +0100
    
        Added nano-fs and babel-parser in prep for async compilation
    
    commit 5ac8c5e9c412bde76ee82747f703f68c5338003a
    Merge: 7181d20 b43c643
    Author: Rob Barns-Graham <r.barnsgraham@reply.com>
    Date:   Thu Mar 30 09:38:31 2017 +0100
    
        Merge branch 'master' into develop
    
    commit 7181d2098abb466c2fb7248ebd5edd362983fd10
    Merge: af33e98 543909c
    Author: Rob BG <rob.bg@me.com>
    Date:   Wed Mar 22 13:58:44 2017 +0000
    
        Merge branch 'master' into develop

--- compile.js
+++ compile.js
@@ -1,12 +1,12 @@
-const fs = require('fs-extended');
+const dateformat = require('handlebars-dateformat');
+const dir = require('node-dir');
+const fs = require('nano-fs');
 const fm = require('front-matter');
 const handlebars = require('handlebars');
-const dateformat = require('handlebars-dateformat');
-const moment = require('moment');
-const dir = require('node-dir');
-const mkpath = require('mkpath');
-const marked = require('marked');
 const inline = require('inline-source');
+const marked = require('marked');
+const moment = require('moment');
+const path = require('path');
 
 // Paths
 const paths = {
@@ -24,11 +24,11 @@ const globalItems = [];
 const markedOpts = { gfm: true };
 
 // Register partials
-const registerPartials = () => {
-  const files = fs.readdirSync(paths.partialPath); // Read partials
+const registerPartials = async () => {
+  const files = await fs.readdir(paths.partialPath); // Read partials
 
-  files.forEach((file) => {
-    const content = fs.readFileSync(`${paths.partialPath}/${file}`, 'utf8');
+  files.forEach(async (file) => {
+    const content = await fs.readFile(`${paths.partialPath}/${file}`, 'utf8');
     const filename = file.substring(0, file.lastIndexOf('.'));
     handlebars.registerPartial(filename, content);
   });
@@ -38,47 +38,61 @@ const registerPartials = () => {
 const registerHelpers = () => handlebars.registerHelper('dateFormat', dateformat);
 
 // Inline css and js
-const inlineSource = html => inline.sync(html, { compress: true });
+const inlineSource = html => new Promise((resolve, reject) => {
+  inline(html, { compress: true }, (err, output) => {
+    if (err) reject(err);
+    else resolve(output);
+  });
+});
 
 // Renders the template using Handlebars
-const renderTemplate = (templatePath, viewData) => {
-  const content = fs.readFileSync(templatePath, { encoding: 'utf-8' }); // Read path
+const renderTemplate = async (templatePath, viewData) => {
+  const content = await fs.readFile(templatePath, { encoding: 'utf-8' }); // Read path
   const template = handlebars.compile(content); // Compile template
   return inlineSource(template(viewData)); // Return inline templated data
 };
 
 // Render and write
-const writeToFile = (template, data, path) => {
-  const HTML = renderTemplate(template, data); // Render template
-  mkpath.sync(path); // Make sure path exists
-  fs.writeFileSync(`${path}/index.html`, HTML, { encoding: 'utf8' }); // Write to file
+const writeToFile = async (template, data, filePath) => {
+  console.log('\u270e Saving', filePath);
+  const HTML = await renderTemplate(template, data); // Render template
+  await fs.mkpath(filePath); // Make sure path exists
+  return fs.writeFile(`${filePath}/index.html`, HTML, { encoding: 'utf8' }); // Write to file
 };
 
 // Builds email content and writes to file.
-const buildSingleHTML = (content) => {
+const buildSingleHTML = async (filePath) => {
+  console.log('\u26A1 Building blog entry from', filePath);
+  const content = await fs.readFile(filePath, { encoding: 'utf8' }); // Read content path
   const data = fm(content); // Front matter the content
   const formattedDate = moment(data.attributes.date).format('YYYY-MM-DD'); // Format date
   const htmlPath = `${paths.buildPath}blog/${formattedDate}-${data.attributes.slug}`; // Build path
   const htmlTemplate = `${paths.templatePath}/${data.attributes.template}`; // Template path
   data.bodyFormatted = marked(data.body, markedOpts); // Converts markdown body into html
   globalItems.push(data); // Push into global array for blog index
-  writeToFile(htmlTemplate, data, htmlPath); // Render the HTML content and write to file
+
+  // Render the HTML content and write to file
+  return writeToFile(htmlTemplate, data, htmlPath);
 };
 
 // Build HTML for homepage
-const buildHomeHTML = () => {
-  const content = fs.readFileSync(`${paths.globalPath}/homepage.md`, { encoding: 'utf8' }); // Read content path
+const buildHomeHTML = async () => {
+  console.log('\u2302 Building home page');
+  const content = await fs.readFile(`${paths.globalPath}/homepage.md`, { encoding: 'utf8' }); // Read content path
   const data = fm(content); // Front matter the content
   const htmlTemplate = `${paths.templatePath}/${data.attributes.template}`; // Template path
   data.bodyFormatted = marked(data.body, markedOpts); // Converts markdown body into html
-  writeToFile(htmlTemplate, data, paths.buildPath); // Render the HTML content and write to file
+
+  // Render the HTML content and write to file
+  return writeToFile(htmlTemplate, data, paths.buildPath);
 };
 
 // Build HTML for blog listing page
-const buildBlogHTML = () => {
-  const content = fs.readFileSync(`${paths.globalPath}/blog.md`, { encoding: 'utf8' }); // Read content path
+const buildBlogHTML = async () => {
+  console.log('\u2630 Building blog index');
+  const content = await fs.readFile(`${paths.globalPath}/blog.md`, { encoding: 'utf8' }); // Read content path
   const data = fm(content); // Front matter the content
-  const htmlTemplate = `${paths.templatePath}/${data.attributes.template}`; // TEmpalte path
+  const htmlTemplate = `${paths.templatePath}/${data.attributes.template}`; // Template path
   data.bodyFormatted = marked(data.body, markedOpts); // Converts markdown body into html
   data.items = globalItems.sort((a, b) => { // Sort posts in alphabetical order
     const aDate = new Date(a.attributes.date);
@@ -87,13 +101,9 @@ const buildBlogHTML = () => {
     else if (aDate < bDate) return 1;
     return 0;
   });
-  writeToFile(htmlTemplate, data, `${paths.buildPath}/blog`); // Render the HTML content and write to file
-};
 
-// Build index pages
-const buildMainPages = () => {
-  buildBlogHTML(); // Blog index page
-  buildHomeHTML(); // Home page
+  // Render the HTML content and write to file
+  return writeToFile(htmlTemplate, data, `${paths.buildPath}blog`);
 };
 
 // Run app
@@ -101,18 +111,16 @@ const buildMainPages = () => {
   registerPartials(); // Register partials
   registerHelpers(); // Register helpers
 
-  // Uses 'node-dir' to recursively search through the contentPath directory.
-  dir.readFiles(
-    paths.contentPath,
-    {
-      match: /.md$/, // Only matches files ending in .md
-      exclude: /^\./, // Excludes all system files
-    },
-    (err, content, next) => {
-      if (err) throw err;
-      buildSingleHTML(content); // Build html for each blog post
-      next(); // Moves on to the next content file
-    },
-    buildMainPages
-  );
+  dir.promiseFiles(paths.contentPath)
+  .then(files => files
+    .filter((file) => { // Filter out any files that don't have .md extension
+      const extname = path.extname(file);
+      return (extname === '.md');
+    })
+    .map(buildSingleHTML) // Map to return array of promises that resolve to the built HTML
+  )
+  .then(posts => Promise.all(posts))
+  .then(() => Promise.all([buildBlogHTML(), buildHomeHTML()]))
+  .then(() => console.log('\u263a All done!'))
+  .catch(e => console.error('\u2620', e));
 })();
